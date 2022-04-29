8e21395066654b3c05e8a6d2ba5534b4b1666b99
Yimin Ji <sylviaji@seas.upenn.edu>
2022-04-29 18:05:32 -0400

    add ip-api call

--- client/src/apis/getBusinesses.js
+++ client/src/apis/getBusinesses.js
@@ -1,15 +1,25 @@
 const axios = require('axios');
 
 async function getBusinesses(query) {
-  const location = '19104';
+  let location = '19104';
   try {
-    const res = await axios.get(`/yelp/${query}/${location}`);
-    if (res.status === 200) {
-      return res.data.businesses;
+    const res1 = await axios.get('http://ip-api.com/json');
+    if (res1.status == 200) {
+      location = res1.data.zip;
     }
-  } catch (err) {
-    console.error(err);
+  } catch (err1) {
+    console.error(err1);
   }
+
+  try {
+    const res2 = await axios.get(`/yelp/${query}/${location}`);
+    if (res2.status === 200) {
+      return res2.data.businesses;
+    }
+  } catch (err2) {
+    console.error(err2);
+  }
+
   return [];
 }
 
