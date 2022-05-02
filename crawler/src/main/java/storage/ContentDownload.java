f57539ac9745f87896a6e177afb34b31a7a81247
Yuchen Ding <yuchending@host-152-99.paphevo.philadelphia.pa.us.clients.pavlovmedia.net>
2022-05-02 16:30:23 -0400

    update data transfer

--- crawler/src/main/java/storage/ContentDownload.java
+++ crawler/src/main/java/storage/ContentDownload.java
@@ -8,55 +8,74 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.regions.Regions;
+
+
 // Download crawled content from RDS for transfering to S3
 public class ContentDownload {
 
     public static void main(String[] args){
         System.out.println("start downloading");
 
+        // Connect to RDS
         StorageFactory.connectToDatabase();
         StorageInterface storage = StorageFactory.getInstance();
 
-        int start = 120001;
-        int numDoc = 1000;
+        // Connect to S3
+        AWSCredentials credentials = new BasicAWSCredentials(
+                "AKIAZFMQMWQOSFESKUCG",
+                "2anUrYUb0yWKKHaJYq6k+ioPkBOGMI3+mE7R4mhz"
+        );
 
-        // In batch of 1,000
-        for (int i = start; i < start + 10000; i += numDoc){
-            int startIdx = i;
-            int endIdx = startIdx + numDoc - 1;
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.US_EAST_1)
+                .build();
 
-            System.out.println("starting index: " + startIdx);
-            System.out.println("ending index: " + endIdx);
 
-            String rootpath = "./rds";
-            String foldername = startIdx + "-" + endIdx;
-            String folderpath = rootpath + "/" + foldername;
-            System.out.println("folderpath: " + folderpath);
+        int startIdx = 180001;
+        int endIdx = 200000;
 
-            File directory = new File(folderpath);
+        String bucketName = "555docbucket";
+        String foldername = startIdx + "-" + endIdx;
 
-            if (! directory.exists()){
-                directory.mkdirs();
-            }
+        int batchSize = 1000;
 
-            HashMap<Integer, String> map = storage.getDocumentByRange(startIdx, numDoc);
+        // Retrieve in batches of 1000
+        for (int i = startIdx; i < endIdx; i += batchSize){
+            System.out.println("startIdx: " + i);
+
+            HashMap<Integer, String> map = storage.getDocumentByRange(i, batchSize);
 
             for (Map.Entry ele: map.entrySet()){
                 int id = (int) ele.getKey();
                 String content = (String) ele.getValue();
 
-                String filename = folderpath + "/" + id;
-                File file = new File(filename);
+                String filename = Integer.toString(id);
+                File file = new File("./rds/" + filename);
 
                 try {
-                    BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("./rds/" + filename));
                     writer.write(content);
 
                     writer.close();
                 } catch (IOException e){
                     e.printStackTrace();
                 }
+
+                s3client.putObject(
+                        bucketName,
+                        "in/" + filename,
+                        new File("./rds/" + filename)
+                );
             }
         }
+
     }
 }
