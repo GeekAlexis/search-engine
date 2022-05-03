f3a7d8ba645f6fb5be4b35f82451398eefa23f64
Yuchen Ding <yuchending@host-152-99.paphevo.philadelphia.pa.us.clients.pavlovmedia.net>
2022-05-02 23:57:58 -0400

    update server

--- server/src/main/java/edu/upenn/cis/cis455/Server.java
+++ server/src/main/java/edu/upenn/cis/cis455/Server.java
@@ -8,8 +8,15 @@ import main.java.edu.upenn.cis.cis455.handlers.YelpHandler;
 
 public class Server {
   public static void main(String[] args) {
+    if (args.length < 2) {
+        System.err.println("Syntax: Server {port} {database url} ");
+        System.exit(1);
+    }
+
+    port(args[0]);
+
     get("/hello", (req, res) -> "Hello World");
-    get("/search", new SearchHandler());
+    get("/search", new SearchHandler(args[1]));
     get("/news", new NewsHandler());
     get("/yelp/:term/:location", new YelpHandler());
   }
--- server/src/main/java/edu/upenn/cis/cis455/handlers/SearchHandler.java
+++ server/src/main/java/edu/upenn/cis/cis455/handlers/SearchHandler.java
@@ -1,4 +1,4 @@
-package main.java.edu.upenn.cis.cis455.handlers;
+package edu.upenn.cis.cis455.handlers;
 
 import spark.Request;
 import spark.Route;
@@ -13,11 +13,27 @@ import java.io.IOException;
 import spark.HaltException;
 import spark.Session;
 
-public class SearchHandler implements Route {
-  @Override
-  public Object handle(Request req, Response res) throws HaltException {
-    String query = req.queryParams("query");
+import edu.upenn.cis.cis455.search.Retrieval;
+import edu.upenn.cis.cis455.search.RetrievalResult;
 
-    return "";
-  }
+public class SearchHandler implements Route {
+	private Retrieval retrieval;
+
+	public SearchHandler(String dbUri) {
+		retrieval = Retrieval(dbUri, 1.2, 0.75, 1.0, 50);
+	}
+
+	@Override
+	public Object handle(Request req, Response res) throws HaltException {
+		String query = req.queryParams("query");
+
+		List<String> terms = retrieval.preprocessQuery(query);
+        List<RetrievalResult> results = retrieval.retrieve(terms, 100);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(results);
+		// {[{url, title, excerpt, bm25, page rank, overall score}, …]}
+
+		return json;
+	}
 }
