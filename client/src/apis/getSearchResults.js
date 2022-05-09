5d8948337945088aa14507d92139868fc3b7b202
Yimin Ji <sylviaji@seas.upenn.edu>
2022-05-09 13:32:55 -0400

    fix null query

--- client/src/apis/getSearchResults.js
+++ client/src/apis/getSearchResults.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 
 async function getSearchResults(query, page) {
+  if (query == null || query.length == 0) {
+    return { match: 0, data: [] };
+  }
   try {
     const res = await axios.get(
       `http://3.239.191.178:4567/search?query=${query}&page=${page}`
