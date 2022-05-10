80dc46c9a249113039ed53980c8f8b1d80a70ffe
Yimin Ji <sylviaji@seas.upenn.edu>
2022-05-10 12:00:04 -0400

    fix yelp route

--- server/src/main/java/edu/upenn/cis/cis455/Server.java
+++ server/src/main/java/edu/upenn/cis/cis455/Server.java
@@ -10,38 +10,38 @@ import org.apache.logging.log4j.Level;
 import static org.apache.logging.log4j.core.config.Configurator.setLevel;
 
 public class Server {
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.err.println("Syntax: Server {port}");
-            System.exit(1);
-        }
-        setLevel("edu.upenn.cis.cis455", Level.DEBUG);
-
-        port(Integer.parseInt(args[0]));
-
-        // ref: https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
-        options("/*", (request, response) -> {
-            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-            if (accessControlRequestHeaders != null) {
-                response.header("Access-Control-Allow-Headers",
-                        accessControlRequestHeaders);
-            }
-
-            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-            if (accessControlRequestMethod != null) {
-                response.header("Access-Control-Allow-Methods",
-                        accessControlRequestMethod);
-            }
-
-            return "OK";
-        });
-
-        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
-
-        get("/search", new SearchHandler());
-        get("/news", new NewsHandler());
-        get("/yelp/:term/:location", new YelpHandler());
-
-        System.out.println("Server launched!");
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.err.println("Syntax: Server {port}");
+      System.exit(1);
     }
+    setLevel("edu.upenn.cis.cis455", Level.DEBUG);
+
+    port(Integer.parseInt(args[0]));
+
+    // ref: https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+    options("/*", (request, response) -> {
+      String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+      if (accessControlRequestHeaders != null) {
+        response.header("Access-Control-Allow-Headers",
+            accessControlRequestHeaders);
+      }
+
+      String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+      if (accessControlRequestMethod != null) {
+        response.header("Access-Control-Allow-Methods",
+            accessControlRequestMethod);
+      }
+
+      return "OK";
+    });
+
+    before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+    get("/search", new SearchHandler());
+    get("/news", new NewsHandler());
+    get("/yelp", new YelpHandler());
+
+    System.out.println("Server launched!");
+  }
 }
